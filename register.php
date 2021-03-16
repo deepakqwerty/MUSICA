ed0f6e86595b88bfeaac770e3bf435691c59c2ac
deepakqwerty <deepakqwerty8@gmail.com>
2021-03-16 21:41:02 +0530

    Update register.php

--- register.php
+++ register.php
@@ -1,5 +1,5 @@
 <?php
-include("includes/handlers/register-handler.php");
+include("includes/handlers/Account.php");
 $account = new Account();
 $account->register();
 include("includes/handlers/register-handler.php");
@@ -51,18 +51,23 @@ function getInputValue($name){
                 <form id="registerForm" action="register.php" method="post">
                     <h2> Create your free account</h2>
                     <p>
+                        <?php echo $account->getError("Your username must be between 5 and 25 characters"); ?>
                         <label for="username">Username</label>
                         <input id="username" name="username" type="text" placeholder="e.g. AshKetchum01" value="<?php getInputValue('username') ?>" required>
                     </p>
                     <p>
+                        <?php echo $account->getError("Your first name must be between 2 and 25 characters"); ?>
                         <label for="firstName">First Name</label>
                         <input id="firstName" name="firstName" type="text" placeholder="e.g. Ash" value="<?php getInputValue('firstName') ?>" required>
                     </p>
                     <p>
+                        <?php echo $account->getError("Your last name must be between 2 and 25 characters"); ?>
                         <label for="lastName">Last Name</label>
                         <input id="lastName" name="lastName" type="text" placeholder="e.g. Ketchum" value="<?php getInputValue('lastName') ?>" required>
                     </p>
                     <p>
+                        <?php echo $account->getError("Your emails don't match"); ?>
+                        <?php echo $account->getError("Email is invalid"); ?>
                         <label for="email">Email</label>
                         <input id="email" name="email" type="email" placeholder="e.g. AshKetchum003@gmail.com" value="<?php getInputValue('email') ?>" required>
                     </p>
@@ -71,6 +76,9 @@ function getInputValue($name){
                         <input id="email2" name="email2" type="email" placeholder="e.g. AshKetchum003@gmail.com" value="<?php getInputValue('email2') ?>" required>
                     </p>
                     <p>
+                        <?php echo $account->getError("Your passwords don't match"); ?>
+                        <?php echo $account->getError("Your password can only contain numbers and letters"); ?>
+                        <?php echo $account->getError("Your password must be between 5 and 30 characters"); ?>
                         <label for="password">Password</label>
                         <input id="password" name="password" type="password"  placeholder="Your Password" required>
                     </p>
