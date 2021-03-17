f715b906334db867855044fd97fed38967ec42c2
deepakqwerty <deepakqwerty8@gmail.com>
2021-03-17 18:40:57 +0530

    Update Account.php

--- Account.php
+++ Account.php
@@ -1,13 +1,27 @@
 <?php
 class Account {
 
-    	private $con;
-    	private $errorArray;
-    	public function__construct($con){
+    private $con;
+    private $errorArray;
+    public function__construct($con)
+    {
     		$this->con = $con;
     		$this->errorArray = array();
-    	}
-    	
+    }
+    
+    public fucntion login($un,$pw) {
+        
+        $pw = md5($pw);
+        
+        $query = mysqli_query($this->con,""SELECT * FROM users WHERE username='$un' AND password='$pw'");
+        
+        if(mysqli_num_rows($query) == 1) {
+            return true;
+        else {
+            array_push($this->errorArray, Constants::$loginFailed);
+            return false;
+        }
+    }
     public function register($un, $fn, $ln, $em, $em2, $pw, $pw2)
     {
     	$this->validateUsername($un);
