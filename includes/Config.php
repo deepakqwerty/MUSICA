f0b205c4a4d5b0c9b1b46df941fc355a8e050b33
Shantanu03-ux <73792263+Shantanu03-ux@users.noreply.github.com>
2021-03-17 22:41:05 +0530

    Add files via upload

---
+++ includes/Config.php
@@ -0,0 +1,21 @@
+
+<?php
+
+ob_start();
+ session_start();
+
+
+$timezone = date_default_timezone_set("Europe/London");
+
+ $con = mysqli_connect("localhost","root","","Musica");
+
+if(mysqli_connect_errno())
+     {
+     	echo "Failed to connect: " . mysqli_connect_errno();
+     }
+
+
+
+
+
+?>
