ae3a26b82b615d46c3617748952b6e55aac4daaf
deepakqwerty <deepakqwerty8@gmail.com>
2021-03-17 18:42:49 +0530

    Update login-handler.php

--- includes/handlers/login-handler.php
+++ includes/handlers/login-handler.php
@@ -4,5 +4,11 @@ if(isset($_POST['loginButton'])){
 	$username = $_POST['loginUsername'];
 	$password = $_POST['loginPassword'];
 	
+	$result = $account->login($username, $password);
+	
+	if($result == true) {
+		header("Location: index.php");
+	}
+	
 }
 ?>
