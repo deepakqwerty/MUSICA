61a8073236b088274e9f8f9f7c884a1ed3f503a0
Shantanu03-ux <73792263+Shantanu03-ux@users.noreply.github.com>
2021-04-10 23:16:27 +0530

    Add files via upload

---
+++ slotify/includes/nowPlayingBar.php
@@ -0,0 +1,86 @@
+<div id="nowPlayingBarContainer">
+    <div id="nowPlayingBar">
+
+            <div id="nowPlayingLeft">
+                <div class="content">
+                    <span class="albumLink">
+                        <img src="assets/images/faded.jpg" class="albumArtwork" alt="albumPicture">
+                    </span>
+
+                    <div class="trackInfo">
+                        <span class="trackName">
+                            <span>Faded</span>
+                        </span>
+
+                        <span class="artistName">
+                            <span>Alan Walker</span>
+                        </span>
+
+                    </div>
+                </div>
+            </div>
+
+            <div id="nowPlayingCenter">
+                <div class="content playerControls">
+                    <div class="buttons">
+                                <button class="controlButton shuffle" title="Shuffle button">
+                                    <img src="assets/images/icons/shuffle.png" alt="shuffle">
+                                </button>
+
+                                <button class="controlButton previous" title="Previous button">
+                                    <img src="assets/images/icons/previous.png" alt="previous">
+                                </button>
+
+                                <button class="controlButton play" title="Play button">
+                                    <img src="assets/images/icons/play.png" alt="play">
+                                </button>
+
+                                <button class="controlButton pause" title="Pause button" style="display:none;">
+                                    <img src="assets/images/icons/pause.png" alt="pause">
+                                </button>
+
+                                <button class="controlButton next" title="Next button">
+                                    <img src="assets/images/icons/next.png" alt="next">
+                                </button>
+
+                                <button class="controlButton repeat" title="Repeat button">
+                                    <img src="assets/images/icons/repeat.png" alt="repeat">
+                                </button>
+                    </div>
+
+                    <div class="playbackBar">
+                            <span class="progressTime current">0.00</span>
+
+                            <div class="progressBar">
+                                <div class="progressBarBg">
+                                    <div class="progress"></div>
+                                </div>
+                            </div>
+
+                            <span class="progressTime remaining">0.00</span>
+                    </div>
+                </div>
+            </div>
+
+                    <div id="nowPlayingRight">
+                        <div class="volumeBar">
+
+                            <button class="controlButton volume" title="Volume Button">
+                                <img src="assets/images/icons/volume.png" alt="Volume">
+                            </button>
+
+                            <div class="progressBar">
+                                <div class="progressBarBg">
+                                    <div class="progress"></div>
+                                </div>
+                            </div>
+
+
+                        </div>
+                    </div>
+
+
+
+    </div>
+
+</div>
